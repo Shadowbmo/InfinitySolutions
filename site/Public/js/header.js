9d793c0ae14198e3607c48a1636acc1e1b830f34
shadowbmo <douglas.silva@sptech.school>
2023-03-15 12:32:02 -0300

    github pages

--- site/Public/js/header.js
+++ site/Public/js/header.js
@@ -4,16 +4,16 @@ function header() {
     <header id="header">
         <nav class="container">
             <a href="./index.html" class="logo">
-                <img src="./assets/img/Monitoring_Solution.svg" alt="">
+                <img src="./site/Public/assets/img/Monitoring_Solution.svg" alt="">
             </a>
 
             <div class="Registro">
-            <a href="./telaLogin.html">
+            <a href="./site/Public/telaLogin.html">
             
             <button> Logar </button>
             </a>
 
-            <a href="./telaLogin.html">
+            <a href="./site/Public/telaLogin.html">
             <button> Registrar </button>
             
             </a>
